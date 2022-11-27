3829419fb86b4972ce25edf12aa536cd025a57fe
Adam Ringhede <adamringhede@live.com>
2022-11-27 01:57:07 +0100

    fix build

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
 import './style.scss'
 import { getReleases, ReleasePlatform } from './releases'
 
-const releases = await getReleases()
-console.log(releases)
+async function updateDownloadLinks() {
+  const releases = await getReleases()
+  console.log(releases)
 
-function updateReleaseLink(platform: ReleasePlatform) {
-  const link = document.querySelector<HTMLAnchorElement>(
-    '#download-' + platform.toLowerCase()
-  )
-  if (link != null) {
-    const latestRelease = releases
-      .reverse()
-      .find((r) => r.platform === platform)
-    if (latestRelease != null) {
-      link.href = latestRelease.link
+  function updateReleaseLink(platform: ReleasePlatform) {
+    const link = document.querySelector<HTMLAnchorElement>(
+      '#download-' + platform.toLowerCase()
+    )
+    if (link != null) {
+      const latestRelease = releases
+        .reverse()
+        .find((r) => r.platform === platform)
+      if (latestRelease != null) {
+        link.href = latestRelease.link
+      }
     }
   }
+
+  updateReleaseLink(ReleasePlatform.macOS)
+  updateReleaseLink(ReleasePlatform.windows)
+  updateReleaseLink(ReleasePlatform.linux)
 }
 
-updateReleaseLink(ReleasePlatform.macOS)
-updateReleaseLink(ReleasePlatform.windows)
-updateReleaseLink(ReleasePlatform.linux)
+updateDownloadLinks()
