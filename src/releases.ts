a8a2cfa17b97a1f2fbebc6b8c3ae0379118b4709
Adam Ringhede <adamringhede@live.com>
2023-07-12 00:52:35 +0200

    msi installer for windows

--- src/releases.ts
+++ src/releases.ts
@@ -63,7 +63,7 @@ function getPlatformFromFileName(fileName: string): ReleasePlatform | null {
   if (/\d+.AppImage$/.test(fileName)) {
     return ReleasePlatform.linux
   }
-  if (/\d+.exe$/.test(fileName)) {
+  if (/\d+.msi$/.test(fileName)) {
     return ReleasePlatform.windows
   }
   return null
